166d68ef91d8b77d96f4204b6e495b35df6e682e
adenali20 <adenali362@gmail.com>
2024-09-23 17:10:43 -0500

    replace # with login

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -13,19 +13,19 @@ function App() {
         <div class="w3-top">
             <div className="w3-bar w3-theme-d2 w3-left-align w3-large">
                 <a className="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
-                <a href="#" className="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i className="fa fa-home w3-margin-right"></i>Logo</a>
-                <a href="#" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="News"><i className="fa fa-globe"></i></a>
-                <a href="#" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Account Settings"><i className="fa fa-user"></i></a>
-                <a href="#" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Messages"><i className="fa fa-envelope"></i></a>
+                <a href="/login" className="w3-bar-item w3-button w3-padding-large w3-theme-d4"><i className="fa fa-home w3-margin-right"></i>Logo</a>
+                <a href="/login" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="News"><i className="fa fa-globe"></i></a>
+                <a href="/login" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Account Settings"><i className="fa fa-user"></i></a>
+                <a href="/login" className="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Messages"><i className="fa fa-envelope"></i></a>
                 <div className="w3-dropdown-hover w3-hide-small">
                     <button className="w3-button w3-padding-large" title="Notifications"><i class="fa fa-bell"></i><span class="w3-badge w3-right w3-small w3-green">3</span></button>     
                     <div class="w3-dropdown-content w3-card-4 w3-bar-block" style={{width:"300px"}}>
-                        <a href="#" className="w3-bar-item w3-button">One new friend request</a>
-                        <a href="#" className="w3-bar-item w3-button">John Doe posted on your wall</a>
-                        <a href="#" className="w3-bar-item w3-button">Jane likes your post</a>
+                        <a href="/login" className="w3-bar-item w3-button">One new friend request</a>
+                        <a href="/login" className="w3-bar-item w3-button">John Doe posted on your wall</a>
+                        <a href="/login" className="w3-bar-item w3-button">Jane likes your post</a>
                     </div>
                 </div>
-                <a href="#" class="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" title="My Account">
+                <a href="/login" class="w3-bar-item w3-button w3-hide-small w3-right w3-padding-large w3-hover-white" title="My Account">
                     <img src="/w3images/avatar2.png" className="w3-circle" style={{height:"23px",width:"23px"}} alt="Avatar"/>
                 </a>
             </div>
@@ -33,10 +33,10 @@ function App() {
 
 {/* <!-- Navbar on small screens --> */}
         <div id="navDemo" className="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
-            <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 1</a>
-            <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 2</a>
-            <a href="#" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
-            <a href="#" className="w3-bar-item w3-button w3-padding-large">My Profile</a>
+            <a href="/login" className="w3-bar-item w3-button w3-padding-large">Link 1</a>
+            <a href="/login" className="w3-bar-item w3-button w3-padding-large">Link 2</a>
+            <a href="/login" className="w3-bar-item w3-button w3-padding-large">Link 3</a>
+            <a href="/login" className="w3-bar-item w3-button w3-padding-large">My Profile</a>
         </div>
     </div>
   );
