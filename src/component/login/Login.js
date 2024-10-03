c075baf76a98c067708e4f4663a1a21af9099678
adenali20 <adenali362@gmail.com>
2024-10-03 14:57:00 -0500

    change domain name

--- src/component/login/Login.js
+++ src/component/login/Login.js
@@ -27,7 +27,7 @@ export default function Login() {
 
 const handleLogin=(un,pwd)=>{
   
-  fetch("http://localhost:8081/api/users/login", {
+  fetch("http://adenmali.com/api/users/login", {
     headers: new Headers({
       "Authorization": `Basic ${encode(`${un}:${pwd}`)}`
     }),
