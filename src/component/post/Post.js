ba6fbd08a02feadaa80e3037c527df969ef33f6e
adenali20 <adenali362@gmail.com>
2024-10-06 12:01:28 -0500

    remove image

--- src/component/post/Post.js
+++ src/component/post/Post.js
@@ -23,12 +23,12 @@ export default function Post({post}) {
     <hr className="w3-clear"/>
     <p>{post?post.text:"random message"}</p>
       <div className="w3-row-padding" style={{"margin":"0 -16px"}}>
-        <div className="w3-half">
+        {/* <div className="w3-half">
           <img src="https://www.w3schools.com/w3images/lights.jpg" style={{"width":"100%"}} alt="Northern Lights" className="w3-margin-bottom"/>
         </div>
         <div className="w3-half">
           <img src="https://www.w3schools.com/w3images/nature.jpg" style={{"width":"100%"}} alt="Nature" className="w3-margin-bottom"/>
-      </div>
+      </div> */}
     </div>
     <button type="button" className="w3-button w3-theme-d1 w3-margin-bottom"><i className="fa fa-thumbs-up"></i>  Like</button> 
     <button type="button" className="w3-button w3-theme-d2 w3-margin-bottom"><i className="fa fa-comment"></i>  Comment</button> 
